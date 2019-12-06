646f56a8c4bcd881e9abee2040602de4f0cf3992
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2019-12-06 17:36:12 -0300

    Add some validation to Post and User models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
+EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: true
 
   def self.new_remember_token
     Digest::SHA1.hexdigest SecureRandom.urlsafe_base64.to_s
