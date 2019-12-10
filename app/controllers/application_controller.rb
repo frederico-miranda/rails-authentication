afea2acc222facb8b90a2b03a714c07b39276728
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2019-12-10 08:32:48 -0300

    Refactor application_controller.rb (move cookie manipulation outside setter, fix bug regarding redirect_to)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,26 +2,22 @@
 
 class ApplicationController < ActionController::Base
   def current_user
-    remember_token = cookies.permanent[:remember_token]
-    @user ||= User.find_by(remember_token: remember_token) if remember_token
-    @user
+    @user ||= User.find_by(remember_token: cookies.permanent[:remember_token])
   end
 
   def current_user=(user)
-    cookies.delete :remember_token if user.nil?
-    cookies.permanent[:remember_token] = user.remember_token unless user.nil?
     @user = user
   end
 
   def sign_in(user)
     user.remember_token = User.new_remember_token
     return unless user.save
-
-    self.current_user = user
-    redirect_to(root_path) && return
+    cookies.permanent[:remember_token] = user.remember_token
+    @user = user
   end
 
   def sign_out
-    self.current_user = nil
+    cookies.delete :remember_token
+    @user = nil
   end
 end
