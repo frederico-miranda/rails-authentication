7ca6fcfa0dc6a90c676ce7cf8094e9e46b200f58
Frederico Miranda Brandão Alves <frederico.miranda.brandao@gmail.com>
2019-12-10 13:16:18 -0300

    Change routes and change paths in the forms of views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get '/posts', to: 'posts#index', as: 'posts'
-  get '/posts/new', to: 'posts#new', as: 'new_post'
-  post '/posts/new', to: 'posts#create'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users/new', to: 'users#create'
+  resources :posts, only: [:new, :create, :index]
+  resources :users, only: [:new, :create]
   get '/sign_in', to: 'sessions#new', as: 'sign_in'
   post '/sign_in', to: 'sessions#create'
   get '/sign_out', to: 'sessions#delete', as: 'sign_out'
